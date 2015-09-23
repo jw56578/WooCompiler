c8e5f0d9fbc65403c2b0f81941498add24343989
jw56578 <jw56578@gmail.com>
2015-09-23 11:06:58 -0500

    add first files

---
+++ WooCompiler.Test/TokenDefinitions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace WooCompiler.Test
+{
+    [TestClass]
+    public class TokenDefinitions
+    {
+        
+
+        [TestMethod]
+        public void IsInteger()
+        {
+            Regex regex = new Regex(WooCompiler.TokenDefinition.INTEGER);
+
+        }
+    }
+}
---
+++ WooCompiler/TokenDefinition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WooCompiler
+{
+    public static class TokenDefinition
+    {
+       
+        public static string INTEGER = @"^[0-9]+$";
+    }
+}
